096850d0339de65f3eb61579b1eeabca2a84b13e
Jaime Burbano <burbanojaime98@gmail.com>
2020-01-07 12:48:39 -0500

    01. Creando nuestra primera página

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default class extends React.Component{
+    render(){
+        return(
+            <>
+                <h1>¡Hola Platzi!</h1>
+                <p>Bienvenidos al curso de Next Js</p>
+            </>
+        )
+    }
+}
