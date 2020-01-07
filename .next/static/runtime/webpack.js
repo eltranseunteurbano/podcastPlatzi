22cd45f46462448e0c6e29fd1c4ba46de4811687
Jaime Burbano <burbanojaime98@gmail.com>
2020-01-07 14:03:44 -0500

    02. Styled JSX

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "523edff0a079d043a79d";
+/******/ 	var hotCurrentHash = "76da7ed12af28be196c9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
